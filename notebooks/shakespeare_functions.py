0b96775739eefb5d7e29bf220c4260d396956fb0
bmathis6 <bmathis6@gatech.edu>
2021-06-02 15:00:57 -0700

    large portion of shakespeare-english dictionary entered

--- notebooks/shakespeare_functions.py
+++ notebooks/shakespeare_functions.py
@@ -5,6 +5,33 @@ import re
 from nltk import RegexpTokenizer
 import shakespeare_dicts as sd
 
+def get_stopwords():
+    return ['ye', 'ay']
+
+def clean_contractions(sentence):
+    
+    contractions = contractions_dict.copy()
+    contractions = {re.sub(r'’', "'", key): re.sub(r'’', "'", value) for key, value in contractions.items() if '.' not in key}
+    contractions = {key.lower():value for key, value in contractions.items() if "'" in key}
+    
+    shakespeare_contractions_dict = sd.shakespeare_contractions_dict()
+    
+    for key, value in shakespeare_contractions_dict.items():
+        contractions[key] = value
+    
+    sentence = clean_punctuation(sentence)
+    new_sentence = ""
+
+    for word in tokenizer.tokenize(sentence):
+        word = word.lower()
+
+        if word in contractions:
+            word = dicto[word]
+ 
+        new_sentence += word + ' '
+
+    return new_sentence
+
 def clean_anachronisms(sentence):
     anachronisms = sd.anachronisms_dict()
     orings = sd.oring_dict()
@@ -26,15 +53,15 @@ def clean_anachronisms(sentence):
         for dicto in all_dicts:
             if word in dicto:
                 word = dicto[word]
-
-            new_sentence += word + ' '
+ 
+        new_sentence += word + ' '
 
     return new_sentence
 
 def clean_punctuation(sentence):
     sentence = re.sub(r"(?<=\A)([.?!-,:;\"\-])+|([.?!-,:;\"\-])+(?=\Z)", ' ', sentence)
     sentence = re.sub(r"(?<=\s)([.?!-,:;\"\-])+|([.?!-,:;\"\-])+(?=\s)", ' ', sentence)
-    return re.sub(r"([.?!-,:;\"\-]){2,}", ' ', sentence)
+    return re.sub(r"([.?!-,:;\"\-]){2,}", ' ', sentence) 
 
 def corpusize(df, column):
     tokenizer = RegexpTokenizer('\S+')
